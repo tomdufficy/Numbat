2dad5a341cb321df244fefb0f977c9d1db5a4ced
Tom Dufficy <63538970+tomdufficy@users.noreply.github.com>
2026-06-09 16:15:05 +0200

    Add NumbatHello command

---
+++ Commands/Modelling/NumbatHelloCommand.cs
@@ -0,0 +1,23 @@
+﻿using Rhino;
+using Rhino.Commands;
+
+namespace Numbat.Commands.Modelling
+{
+    public class NumbatHelloCommand : Command
+    {
+        public NumbatHelloCommand()
+        {
+            Instance = this;
+        }
+
+        public static NumbatHelloCommand Instance { get; private set; }
+
+        public override string EnglishName => "NumbatHello";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RhinoApp.WriteLine("Numbat loaded successfully.");
+            return Result.Success;
+        }
+    }
+}
