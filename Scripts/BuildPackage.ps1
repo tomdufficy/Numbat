ce7dece18b7dd4246625d42e0458708b8c8797c9
Tom Dufficy <63538970+tomdufficy@users.noreply.github.com>
2026-06-10 10:31:11 +0200

    Add automated Yak packaging workflow

--- Scripts/BuildPackage.ps1
+++ Scripts/BuildPackage.ps1
@@ -13,10 +13,17 @@ New-Item -ItemType Directory -Path $PackageDir | Out-Null
 New-Item -ItemType Directory -Path (Join-Path $PackageDir "net48") | Out-Null
 New-Item -ItemType Directory -Path (Join-Path $PackageDir "net7.0") | Out-Null
 
+Copy-Item (Join-Path $Root "Icons\Numbat.png") (Join-Path $PackageDir "icon.png")
+
 Copy-Item (Join-Path $Root "bin\Release\net48\Numbat.rhp") (Join-Path $PackageDir "net48\Numbat.rhp")
 Copy-Item (Join-Path $Root "bin\Release\net7.0\Numbat.rhp") (Join-Path $PackageDir "net7.0\Numbat.rhp")
 
 Push-Location $PackageDir
 yak spec
+$ManifestPath = Join-Path $PackageDir "manifest.yml"
+$Manifest = Get-Content $ManifestPath -Raw
+$Manifest = $Manifest -replace "url: <url>", "url: https://github.com/tomdufficy/Numbat"
+$Manifest += "`r`nicon: icon.png`r`n"
+Set-Content $ManifestPath $Manifest
 yak build
 Pop-Location
