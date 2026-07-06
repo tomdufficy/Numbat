64338fff0c0eac5db4e9eb39e7061c01f0f287b9
Tom Dufficy <63538970+tomdufficy@users.noreply.github.com>
2026-07-06 13:07:20 +0200

    Trim version before tagging

--- scripts/PublishRelease.ps1
+++ scripts/PublishRelease.ps1
@@ -19,7 +19,7 @@ if ($status) {
 git fetch origin
 
 [xml]$csproj = Get-Content $csprojPath
-$version = $csproj.Project.PropertyGroup.Version
+$version = $csproj.Project.PropertyGroup.Version.Trim()
 
 if (-not $version) {
     throw "Could not find <Version> in $csprojPath"
